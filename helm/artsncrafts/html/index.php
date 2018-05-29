1889be2fde724f058e5866de5c6a5813a846ce25
Unomisan <yunomie@gmail.com>
2018-05-28 22:58:11 -0400

    Update index.php with...
    ... changes currently useful but to discard later,

--- helm/artsncrafts/html/index.php
+++ helm/artsncrafts/html/index.php
@@ -1,3 +1,10 @@
 <?php
     phpinfo();
+    echo "<p>Dave was here!</p>";
+    echo "<p>When I tried to fork the page, I get this message in blue at the top:</p>";
+    echo "<pre>You're editing a file in a project you don't have write access to.
+            We've created a fork of this project for you to commit your proposed
+            changes to. Submitting a change to this file will write it to a new
+            branch in your fork, so you can send a pull request.</pre>";
+    echo "<p>Is that what you intended?";
 ?>
