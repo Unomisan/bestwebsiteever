faf00aeb940c236e3d0ae862637eda1cb76f4526
Unomisan <yunomie@gmail.com>
2018-05-28 23:02:48 -0400

    Update index.php

--- helm/artsncrafts/html/index.php
+++ helm/artsncrafts/html/index.php
@@ -1,5 +1,4 @@
 <?php
-    phpinfo();
     echo "<p>Dave was here!</p>";
     echo "<p>When I tried to fork the page, I get this message in blue at the top:</p>";
     echo "<pre>You're editing a file in a project you don't have write access to.
@@ -7,4 +6,5 @@
             changes to. Submitting a change to this file will write it to a new
             branch in your fork, so you can send a pull request.</pre>";
     echo "<p>Is that what you intended?";
+    phpinfo();
 ?>
