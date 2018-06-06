cfec4a12161e8de1bb9bf2e17b7a3e8b829f53b6
Unomisan <yunomie@gmail.com>
2018-06-05 22:04:15 -0400

    Testing whether I can place a file in dev

--- helm/artsncrafts/html/index.php
+++ helm/artsncrafts/html/index.php
@@ -1,3 +1,24 @@
 <?php
-    phpinfo();
+	require_once 'assets/functions.php';
+	require_once 'assets/header.php';
+	require_once 'assets/search.php';
+
+	echo "			<div id='main'>";
+			
+		$sql = 'SELECT * FROM hats;';
+		$result = $pdo->query($sql);
+		echo "\n				<ul>\n";
+		while ($hat = $result->fetch()) {
+			echo "					<li><strong>".$hat['name']."</strong><br>".
+										"<strong>Style:</strong> ".$hat['style']." ".
+										"<strong>from</strong> ".$hat['label']."<br>".
+										"<strong>Crown height:</strong> ".$hat['crown']." ".
+										"<strong>Brim width:</strong> ".$hat['brim']."<br>".
+										"<strong>Description:</strong><br>".$hat['description']."</li><br>\n";
+		}
+		echo "				</ul>\n";
+		
+	echo "			</div>\n";
+			
+	require_once 'assets/footer.php';
 ?>
